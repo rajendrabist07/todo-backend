743c703b3e523a183b1ab7ab9390093949482332
rajendra bist <rajendrabist396@gmail.com>
2026-05-11 16:32:06 +0530

    complete process for this app

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 // Middleware
 
 app.use(cors({
-    origin: "http://127.0.0.1:5173",
+    origin: process.env.frontend_url,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
